3c3b6b6b83810cdbe8b1ee83151b63d1cdddf1af
cocktailcoder <iokreddit@protonmail.com>
2021-07-12 10:58:12 -0400

    search input dynamically filters

---
+++ src/components/App.js
@@ -0,0 +1,59 @@
+import Search from "./Search";
+
+import {useEffect, useState} from "react";
+
+function App() {
+  //holds api data
+  const [myCryptoFeed, setMyCryptoFeed] = useState([])
+  //update search usestate so we can filter
+  const [search, setSearch] = useState('')
+
+  // Fetch Crypto Data 
+  useEffect(()=> {
+    fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024hr%2C%207d%2C%2030d%2C%20200d%2C%201yr`)
+    .then(r => r.json())
+    .then(cryptoData => setMyCryptoFeed(cryptoData))
+  }, [])
+
+const handleChange = (e) => {
+  setSearch(e.target.value)
+}
+//core code to update crypto list against search value using (e.target.value) on input box
+const searchFilterCrypto = myCryptoFeed.filter(coin => 
+  coin.name.toLowerCase().includes(search.toLowerCase())
+);
+
+
+return (
+  <div className ="app">
+    <form>
+        <input onChange = {handleChange}
+        type="text" placeholder="Search Crypto"/>
+    </form>
+
+     <div>
+       {searchFilterCrypto.map(coin => {
+         return (
+          <Search 
+          key = {coin.id}
+          name = {coin.name}
+          image = {coin.image}
+          />
+       )
+       })
+       }
+     </div>
+
+    </div>
+  );
+}
+
+export default App;
+  //get upcoming crpyto events
+
+  // useEffect(()=> {
+  //   fetch(`https://api.coingecko.com/api/v3/events`)
+  //   .then(r => r.json())
+  //   .then(data => console.log(data))
+  // }, [])
+
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+function Search ({name, image}){
+    
+return (
+    <>
+    <div>
+        <img src={image} alt='crypto'/>
+        <h1>{name}</h1>
+    </div>
+    </>
+)
+}
+export default Search
